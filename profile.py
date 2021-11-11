82d6b2e3a2077ea5267689e0a9ea4130be1247f4
Dustin Maas <dmaas@flux.utah.edu>
2021-11-10 18:59:09 -0700

    Initial commit.

---
+++ profile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+"""
+A single compute node with an image that includes docker, docker-compose,
+tshark, oai-cn5g-fed v1.2.1, and docker images for all of the OAI 5G core
+network functions.
+
+"""
+
+import geni.portal as portal
+import geni.rspec.pg as rspec
+import geni.urn as URN
+import geni.rspec.emulab.pnext as PN
+
+request = portal.context.makeRequestRSpec()
+
+node = request.RawPC( "node" )
+node.hardware_type = "d430"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+OAI2021FallWS:oai-cn5g-docker"
+
+portal.context.printRequestRSpec()
