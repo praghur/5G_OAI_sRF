0b31fdfb91a54ecd06f3178c13cdcd64158f86be
Dustin Maas <dmaas@flux.utah.edu>
2021-12-03 18:48:44 -0700

    Add helper for OAI clone command.

---
+++ bin/clone-oai.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /local/repository/bin/typer.sh
+typer "git clone --branch 2021.w46-powder --depth 1 \
+https://gitlab.flux.utah.edu/powder-mirror/openairinterface5g $HOME/openairinterface5g"
