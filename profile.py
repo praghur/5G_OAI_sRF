e82ea56c53a65fb4851dbd757efcb031f2b6cad3
Dustin Maas <dmaas@flux.utah.edu>
2021-12-07 21:32:18 -0700

    Add link to instructions for OAI workshop.

--- profile.py
+++ profile.py
@@ -5,6 +5,10 @@ A single compute node with an image that includes docker, docker-compose,
 tshark, oai-cn5g-fed v1.2.1, and docker images for all of the OAI 5G core
 network functions.
 
+For use by OAI Fall 2021 Workshop RAN Lab sessions. Instructions located at:
+
+https://gitlab.flux.utah.edu/powderrenewpublic/oai_fall_2021_workshop
+
 """
 
 import geni.portal as portal
