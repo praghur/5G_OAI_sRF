08b89acb1f57920ab74da5d4dd642d3aae5a833f
Dustin Maas <dmaas@flux.utah.edu>
2023-05-18 22:58:48 -0600

    Update docs for MERIF.

--- profile.py
+++ profile.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 
 tourDescription = """
-### MWW 2023: OAI 5G RAN/Core (simulated RF)
+### MERIF 2023: Teaching 5G with POWDER
 
-This profile is for an OAI 5G RAN/Core hands-on session during MWW 2023. It
+This profile is for an educational tutorial session during MERIF 2023. It
 deploys a single compute node with an image that includes docker,
 docker-compose, tshark, oai-cn5g-fed v1.4.0, and docker images for all of the
 OAI 5G core network functions. It also includes source code and a prebuilt
-version of the OAI RAN stack (gNB, nrUE, RF simulator). Each attendee will
-deploy this profile and use a web-based VNC client to interact with their
-experiment. The description and instructions for this activity can be found
-[here](https://gitlab.flux.utah.edu/powderrenewpublic/mww2023/-/blob/main/content/oai.md).
+version of the OAI RAN stack (gNB, nrUE, RF simulator). We will show how
+profiles like this one can be used in the classroom to explore 5G networks.
+While this session won't be a hands-on exercice, attendees will be able to try
+it out for themselves after the session, and use it as a starting point for
+teaching 5G concepts in their own classrooms. The description and instructions
+for this activity can be found
+[here](https://gitlab.flux.utah.edu/powderrenewpublic/merif2023/-/blob/main/content/teaching-5g-oai.md).
 """
 
 tourInstructions = """
 
-Note: After you instantiate your experiment, you have to wait until the POWDER
-portal shows your experiment status in green as "Your experiment is ready!"
+Note: After you instantiate an experiment, you have to wait until the POWDER
+portal shows your experiment status in green as "Your experiment is ready"
 before proceeding.
 
 
