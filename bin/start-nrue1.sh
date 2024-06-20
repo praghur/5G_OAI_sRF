54c1a021c8a8d0ef3524e47c49bd4f10e67b1db5
praghur <166060957+praghur@users.noreply.github.com>
2024-06-20 11:37:42 -0400

    Update start-nrue1.sh

--- bin/start-nrue1.sh
+++ bin/start-nrue1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /local/repository/bin/typer.sh
-typer "sudo RFSIMULATOR=127.0.0.1 $HOME/openairinterface5g/cmake_targets/ran_build/build/nr-uesoftmodem -r 106 \
---numerology 1 --band 78 -C 3619200000 --rfsim --sa --nokrnmod -d -O /local/repository/etc/ue1.conf"
+typer "sudo RFSIMULATOR=127.0.0.1 $HOME/openairinterface5g/cmake_targets/ran_build/build/nr-uesoftmodem -r 106 \ --band 78 -C 3619200000 --rfsim --sa --nokrnmod -d -O /local/repository/etc/ue1.conf"
