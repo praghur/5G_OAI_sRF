3eee869176a5f50b1e8ac691e5ac3b57fb8c021f
praghur <166060957+praghur@users.noreply.github.com>
2024-06-20 11:37:56 -0400

    Update and rename start-gnb.sh to start-gnb1.sh

--- bin/start-gnb1.sh
+++ bin/start-gnb1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /local/repository/bin/typer.sh
 typer "sudo RFSIMULATOR=server $HOME/openairinterface5g/cmake_targets/ran_build/build/nr-softmodem --rfsim \
---sa -d -O /local/repository/etc/gnb.conf"
+--sa -d -O /local/repository/etc/gnb1.conf"
